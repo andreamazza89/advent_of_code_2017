be9d796a377b50d857d99208d07a059c373e877f
andreamazza89 <andrea.mazzarella89@gmail.com>
2017-12-05 21:58:26 +0000

    day 5 part two

--- lib/year_2017.five.ex
+++ lib/year_2017.five.ex
@@ -1,38 +1,75 @@
 defmodule Year2017.Five do
 
   def jump(state = %{ instructions: instructions,
-                      current_instruction: current_instruction,
+                      cursor: cursor,
                       jumps_made: jumps_made }) do
     %{ state |
-         instructions: update_instructions(instructions, current_instruction),
-         current_instruction: update_current_instruction(instructions, current_instruction),
+         instructions: update_instructions(instructions, cursor),
+         cursor: update_current_instruction(instructions, cursor),
          jumps_made: jumps_made + 1 }
   end
 
-  defp update_instructions(instructions, current_instruction) do
-    new_instruction = Enum.at(instructions, current_instruction) + 1
-  	instructions
-			|> List.replace_at(current_instruction, new_instruction)
+  def jump_two(state = %{ instructions: instructions,
+													cursor: cursor,
+													jumps_made: jumps_made }) do
+    %{ state |
+         instructions: update_instructions_part_two(instructions, cursor),
+         cursor: update_current_instruction(instructions, cursor),
+         jumps_made: jumps_made + 1 }
   end
 
-	defp update_current_instruction(instructions, current_instruction) do
-		current_instruction + Enum.at(instructions, current_instruction)
+  defp update_instructions(instructions, cursor) do
+    new_instruction = Enum.at(instructions, cursor) + 1
+  	instructions
+			|> List.replace_at(cursor, new_instruction)
+  end
+
+  defp update_instructions_part_two(instructions, cursor) do
+		current_instruction_value = Enum.at(instructions, cursor)
+		new_instruction = if (current_instruction_value >= 3) do
+			current_instruction_value - 1
+		else
+			current_instruction_value + 1
+		end
+  	instructions
+			|> List.replace_at(cursor, new_instruction)
+  end
+
+	defp update_current_instruction(instructions, cursor) do
+		cursor + Enum.at(instructions, cursor)
 	end
 
   def part_one(instructions) do
     initial_state = %{ instructions: instructions,
-                       current_instruction: 0,
+                       cursor: 0,
                        jumps_made: 0 }
     do_part_one(initial_state)
   end
 
   def do_part_one(state = %{ instructions: instructions,
-                      current_instruction: current_instruction,
+                      cursor: cursor,
                       jumps_made: jumps_made }) do
-    if (current_instruction >= length(instructions)) do
+    if (cursor >= length(instructions)) do
   		jumps_made
     else
 			do_part_one(jump(state))
+    end
+  end
+
+  def part_two(instructions) do
+    initial_state = %{ instructions: instructions,
+                       cursor: 0,
+                       jumps_made: 0 }
+    do_part_two(initial_state)
+  end
+
+  def do_part_two(state = %{ instructions: instructions,
+														 cursor: cursor,
+														 jumps_made: jumps_made }) do
+    if (cursor >= length(instructions)) do
+  		jumps_made
+    else
+			do_part_two(jump_two(state))
     end
   end
 
--- test/year_2017_five_test.exs
+++ test/year_2017_five_test.exs
@@ -3,15 +3,15 @@ defmodule Year2017Test.Five do
 
   test "jumps to the next position based on the current instruction" do
     result = %{ instructions: [0, 3, 0, 1, -3],
-                current_instruction: 1,
+                cursor: 1,
                 jumps_made: 0 }
       |> Year2017.Five.jump
-    assert result.current_instruction == 4
+    assert result.cursor == 4
   end
 
   test "updates the count of jumps made every jump" do
     result = %{ instructions: [0, 3, 0, 1, -3],
-                current_instruction: 1,
+                cursor: 1,
                 jumps_made: 0 }
       |> Year2017.Five.jump
     assert result.jumps_made == 1
@@ -19,19 +19,44 @@ defmodule Year2017Test.Five do
 
   test "adds one to the offset of an instruction when it visits it" do
     result = %{ instructions: [0, 3, 0, 1, -3],
-                current_instruction: 1,
+                cursor: 1,
                 jumps_made: 0 }
       |> Year2017.Five.jump
     assert result.instructions == [0, 4, 0, 1, -3]
   end
 
-  test "solves part one" do
-    IO.puts "solution to day 5 part one:"
-    File.read!('./lib/inputs/Five.input')
-      |> String.split("\n", trim: true)
-      |> Enum.map(&String.to_integer/1)
-      |> Year2017.Five.part_one
-      |> IO.inspect
+  test "(part two) decreases one to the offset of an instruction if it's 3 or more" do
+    result = %{ instructions: [0, 3, 0, 1, -3],
+                cursor: 1,
+                jumps_made: 0 }
+      |> Year2017.Five.jump_two
+    assert result.instructions == [0, 2, 0, 1, -3]
   end
+
+  test "(part two) increases one to the offset of an instruction if it's less than 3" do
+    result = %{ instructions: [0, 3, 0, 1, -3],
+                cursor: 2,
+                jumps_made: 0 }
+      |> Year2017.Five.jump_two
+    assert result.instructions == [0, 3, 1, 1, -3]
+  end
+
+ # test "solves part one" do
+ #   IO.puts "solution to day 5 part one:"
+ #   File.read!('./lib/inputs/Five.input')
+ #     |> String.split("\n", trim: true)
+ #     |> Enum.map(&String.to_integer/1)
+ #     |> Year2017.Five.part_one
+ #     |> IO.inspect
+ # end
+
+ #test "solves part two" do
+ #  IO.puts "solution to day 5 part two:"
+ #  File.read!('./lib/inputs/Five.input')
+ #    |> String.split("\n", trim: true)
+ #    |> Enum.map(&String.to_integer/1)
+ #    |> Year2017.Five.part_two
+ #    |> IO.inspect
+ #end
 
 end
