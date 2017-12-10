86e73631cd69c15c2bd2dc88292a5750efd09df1
andreamazza89 <andrea.mazzarella89@gmail.com>
2017-12-10 17:47:50 +0000

    day 10

--- lib/year_2017.ten.ex
+++ lib/year_2017.ten.ex
@@ -1,4 +1,5 @@
 defmodule Year2017.Ten do
+  use Bitwise
 
   def part_one(lengths) do
     do_part_one(Enum.to_list(0..255), 0, 0, lengths)
@@ -18,6 +19,34 @@ defmodule Year2017.Ten do
     do_part_one(new_list, new_position, new_skip_size, rest_lengths)
   end
 
+  def part_two(lengths) do
+    decimal_to_hex_string = fn(n) -> Integer.to_string(n, 16) end
+    pad_hex_with_zero = fn(hex_string) -> String.pad_leading(hex_string, 2, "0") end
+    sparse_hash(Enum.to_list(0..255), 0, 0, 63, lengths, lengths)
+      |> Enum.chunk_every(16)
+      |> Enum.map(&densify/1)
+      |> Enum.map(decimal_to_hex_string)
+      |> Enum.map(pad_hex_with_zero)
+      |> Enum.join
+      |> String.downcase
+  end
+
+  def sparse_hash(list, _, _, 0, [], _) do
+    list
+  end
+
+  def sparse_hash(list, current_position, skip_size, rounds_remaining, [], original_lengths) do
+    sparse_hash(list, current_position, skip_size, rounds_remaining - 1, original_lengths, original_lengths)
+  end
+
+  def sparse_hash(list, current_position, skip_size, rounds_remaining, [ first_length | rest_lengths ], original_lengths) do
+    new_list = wrap_reverse(list, current_position, first_length)
+    new_position = rem(current_position + first_length + skip_size, length(list))
+    new_skip_size = skip_size + 1
+
+    sparse_hash(new_list, new_position, new_skip_size, rounds_remaining, rest_lengths, original_lengths)
+  end
+
   def wrap_reverse(list, current_position, sublist_size) do
     list_starting_with_current_position = rotate(list, current_position)
 
@@ -28,12 +57,17 @@ defmodule Year2017.Ten do
     derotate(reversed ++ untouched, current_position)
   end
 
-  def rotate(list, current_position) do
+  def densify(block) do
+    block
+      |> Enum.reduce(0, fn(x, acc) -> x ^^^ acc end)
+  end
+
+  defp rotate(list, current_position) do
     {afterr, before} = Enum.split(list, current_position)
     before ++ afterr
   end
 
-  def derotate(list, current_position) do
+  defp derotate(list, current_position) do
     {afterr, before} = Enum.split(list, length(list) - current_position)
     before ++ afterr
   end
--- test/year_2017_test_test.exs
+++ test/year_2017_test_test.exs
@@ -3,7 +3,7 @@ defmodule Year2017Test.Ten do
 
   import Year2017.Ten
 
-  test " reverses a sublist that does not wrap" do
+  test "reverses a sublist that does not wrap" do
     result = [0, 1, 2, 3, 4]
       |> wrap_reverse(0, 3)
 
@@ -21,6 +21,27 @@ defmodule Year2017Test.Ten do
     IO.puts "solution to day 10 part one"
     [18,1,0,161,255,137,254,252,14,95,165,33,181,168,2,188]
       |> part_one
+      |> IO.inspect
+  end
+
+  test "densifies a block" do
+    result = [65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22]
+      |> densify
+
+    assert result == 64
+  end
+
+  test "hashes AoC 2017" do
+    result = 'AoC 2017' ++ [17, 31, 73, 47, 23]
+      |> part_two
+
+    assert result == "33efeb34ea91902bb2f59c9920caa6cd"
+  end
+
+  test "solves part two" do
+    IO.puts "solution to day 10 part two"
+    '18,1,0,161,255,137,254,252,14,95,165,33,181,168,2,188' ++ [17, 31, 73, 47, 23]
+      |> part_two
       |> IO.inspect
   end
 end
